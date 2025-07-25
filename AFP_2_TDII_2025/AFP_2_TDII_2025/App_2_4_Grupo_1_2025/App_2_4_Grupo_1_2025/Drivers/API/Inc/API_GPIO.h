3a8d8636ea044d33a18054aa4fcd626351686c73
unknown <daalelucero@gmail.com>
2025-07-25 15:13:32 -0300

    Vuelvo a agregar carpeta AFP_2_TDII_2025

---
+++ AFP_2_TDII_2025/AFP_2_TDII_2025/App_2_4_Grupo_1_2025/App_2_4_Grupo_1_2025/Drivers/API/Inc/API_GPIO.h
@@ -0,0 +1,18 @@
+/*
+ * API_GPIO.h
+ *
+ *  Created on: Jul 25, 2025
+ *      Author: victo
+ */
+
+#ifndef API_INC_API_GPIO_H_
+#define API_INC_API_GPIO_H_
+
+ void MX_ETH_Init(void);
+ void MX_USART3_UART_Init(void);
+ void MX_USB_OTG_FS_PCD_Init(void);
+ void Secuencia1(void);
+ void Secuencia2(void);
+ void Secuencia3(void);
+ void Secuencia4(void);
+#endif /* API_INC_API_GPIO_H_ */
